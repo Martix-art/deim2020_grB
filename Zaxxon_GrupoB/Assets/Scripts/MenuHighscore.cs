fa845268e42fae7c937cf5ee1fde524cfec7a577
Martix-art <leo.enc.marta@gmail.com>
2021-01-28 13:03:24 +0100

    UI
    Interfaz de incio y de high score

---
+++ Zaxxon_GrupoB/Assets/Scripts/MenuHighscore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHighscore : MonoBehaviour
+{
+    public void Volver()
+    {
+        SceneManager.LoadScene("Menu_Inicio");
+    }
+
+   
+}
---
+++ Zaxxon_GrupoB/Assets/Scripts/MenuPlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPlay : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("zaxxon_scene1");
+    }
+
+    public void HIGHSCORE()
+    {
+        SceneManager.LoadScene("Menu_HighScore");
+
+    }
+}
+
